23328e6ea539f51590b4db401b44f91f00187d56
Johoxtum <Johoxtum@hotmail.com>
2023-10-24 19:01:05 -0500

    :recycle: Ajuste en el service

--- src/main/java/com/backend/domain/service/Impl/UserServiceImpl.java
+++ src/main/java/com/backend/domain/service/Impl/UserServiceImpl.java
@@ -2,8 +2,12 @@ package com.backend.domain.service.Impl;
 
 import com.backend.domain.entity.User;
 import com.backend.domain.repository.UserRepository;
+import com.backend.web.dto.LoginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 
 @Service
@@ -12,10 +16,33 @@ public class UserServiceImpl {
     @Autowired
     private UserRepository userRepository;
 
-    public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public ResponseEntity<Object> getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        ResponseEntity<Object> response;
+
+        if (user == null) {
+            String errorMessage = "Usuario no encontrado para el correo electrónico: " + email;
+            response = new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        } else {
+            response = new ResponseEntity<>(user, HttpStatus.OK);
+        }
+
+        return response;
     }
 
 
+    public ResponseEntity<String> authenticateUser(LoginDTO loginDTO) {
+        User user = userRepository.findByEmail(loginDTO.getEmail());
+        ResponseEntity<String> response;
+
+        if (user != null && user.getPassword().equals(loginDTO.getPassword())) {
+            response =  ResponseEntity.ok("Inicio de sesión exitoso");
+        } else {
+            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+        }
+        return response;
+    }
 
 }
+
+
